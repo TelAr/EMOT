f209fef0b86021da3d82311ca819764f1286fa9b
TelAr <qet112313@gmail.com>
2022-06-22 09:45:03 +0900

    레이어 오더 스크립트 생성 및 일부 이펙트 적용
    레이어 순서 이슈가 존재하는 오브젝트에 대해 해당 스크립트 적용으로 순서 조정 가능

---
+++ Assets/Script/LayerOrder.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LayerOrder : MonoBehaviour
+{
+    //Script to fixed layer order
+
+    [Tooltip("Smaller value is front, value must be bigger than (-100)"), Range(-100f, 100f)]
+    public float value = 0f;
+
+    private void LateUpdate()
+    {
+        transform.position = new Vector3(transform.position.x, transform.position.y, value*0.01f);
+    }
+}
