89bf79e5b1db3e228ce3a87fc05cccc5bdac0ad9
TelAr <qet112313@gmail.com>
2022-06-30 09:08:34 +0900

    플레이어 움직임 로직 수정
    마찰 계수를 없애는 방법으로 수직 벽 굳는 이슈 해결
    
    해당 처리 방법으로 인해 지상에서의 속도 소폭 상승

--- Assets/Script/WorldLogic/Camera/CameraTargetting.cs
+++ Assets/Script/WorldLogic/Camera/CameraTargetting.cs
@@ -9,7 +9,7 @@ public class CameraTargetting : MonoBehaviour
 
     public Vector3 Offset;
     public GameObject target;
-    public float SmoothTime = 0.5f;
+    public float SmoothTime = 0.3f;
 
     private Vector3 vel = Vector3.zero;
     // Start is called before the first frame update
@@ -21,12 +21,11 @@ public class CameraTargetting : MonoBehaviour
 
     private void FixedUpdate()
     {
-        if (!IsCameraPositionFix) {
-
+        if (!IsCameraPositionFix)
+        {
             transform.position = Vector3.SmoothDamp(transform.position, Offset + (target.GetComponent<PlayerPhysical>() ?
-                target.GetComponent<PlayerPhysical>().TargettingPos 
+                target.GetComponent<PlayerPhysical>().TargettingPos
                 : target.transform.position), ref vel, SmoothTime);
-
 
         }
     }
