f3ebc2832c633f02eaa624bb40a031a2b41ce86c
sk2000103 <sk2000103@naver.com>
2022-07-06 16:08:30 +0900

    sound_manager 테스트, Resources폴더 이름 오타 수정

---
+++ Assets/Script/Unit/Enemy/Fear/Sound_Class.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sound_Class : AudioDefault
+{
+    private string Name;
+    private AudioClip Clip;
+    private float Volume;
+    private float Time;
+    
+    
+    public Sound_Class(string name, AudioClip clip, float Volume = 0.5f, float time = 0.05f, float VolumeOffset = 1f)
+    {
+        this.Name = name;
+        this.Clip = clip;
+        this.Time = time;
+        this.Volume = Volume * MasterVolume * EffectVolume * VolumeOffset;
+    }
+    
+    public void play()
+    {
+        AudioController controller = GetAudioController();
+
+        AudioSource mAudioSource = controller.Audio;
+        mAudioSource.Stop();
+        mAudioSource.clip = this.Clip;
+        mAudioSource.time = this.Time;
+        mAudioSource.volume = this.Volume;
+        mAudioSource.Play();
+
+        controller.StartTiming = mAudioSource.time;
+        controller.PlayTime = FULLTIME;
+    }
+
+    public string GetName()
+    {
+        return Name;
+    }
+}
---
+++ Assets/Script/Unit/Enemy/Fear/Sound_manager.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+public class Sound_manager : MonoBehaviour
+{
+    public Object sound_path;
+
+    public List<Sound_Class> sounds;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        string path = AssetDatabase.GetAssetPath(sound_path);
+        sounds = load_audio(path);
+    }
+
+    private List<Sound_Class> load_audio(string obj_path)
+    {
+        string[] folders = Directory.GetFiles(obj_path);
+        List<Sound_Class> list = new();
+
+        foreach (string folder in folders)
+        {
+            int len = folder.Length - 4;
+            if (!Equals(folder[len..], "meta"))
+            {
+                if (Equals(AssetDatabase.GetMainAssetTypeAtPath(folder).Name, "DefaultAsset"))
+                {
+
+                }
+                else if (Equals(AssetDatabase.GetMainAssetTypeAtPath(folder).Name, "AudioClip"))
+                {
+                    string temp_path = folder.Replace("Assets/Resources/", "").Split(".")[0].Replace("\\", "/");
+                    string[] split_path = temp_path.Split("/");
+                    Sound_Class clip = new Sound_Class(split_path[split_path.Length-1], Resources.Load<AudioClip>(temp_path));
+                    list.Add(clip);
+                }
+            }
+        }
+        return list;
+    }
+
+    public bool Play(string sound_name)
+    {
+        bool result = false;
+        foreach(Sound_Class temp_sound_class in sounds)
+        {
+            if(Equals(temp_sound_class.GetName(), sound_name))
+            {
+                temp_sound_class.play();
+                result = true;
+            }
+        }
+        return result;
+    }
+}
+ 
