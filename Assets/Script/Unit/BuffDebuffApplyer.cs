77dca3d81e287ccecc62d845374c705271a40ba5
TelAr <qet112313@gmail.com>
2022-07-16 01:01:55 +0900

    :sparkle: 추가: 버프디버프어플라이어 추가
    해당 스크립트는 추후 버프/디버프를 받는 오브젝트에 추가 예정

--- Assets/Script/Unit/BuffDebuffApplyer.cs
+++ Assets/Script/Unit/BuffDebuffApplyer.cs
@@ -4,16 +4,33 @@ using UnityEngine;
 
 public class BuffDebuffApplyer : MonoBehaviour
 {
-    
+    private HashSet<BuffDebuff> buffDebuffs=new();
 
-    void Start()
-    {
-        
+
+    public void AddBuffDebuff(BuffDebuff input) {
+
+        input.Attached = this.gameObject;
+        buffDebuffs.Add(input);
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        
+
+        List<BuffDebuff> destoryList = new List<BuffDebuff>();
+
+        foreach (BuffDebuff bd in buffDebuffs) {
+            if (!bd.Tick()) {
+
+                destoryList.Add(bd);
+            }
+        }
+
+        foreach(BuffDebuff bd in destoryList) 
+        {
+
+            buffDebuffs.Remove(bd);
+        }
+
     }
 }
