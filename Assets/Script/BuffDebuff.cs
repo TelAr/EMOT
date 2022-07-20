4208c860b5c3649b16fe1c6c464990e659bbd6ef
TelAr <qet112313@gmail.com>
2022-07-20 18:01:09 +0900

    :construction: 수정: 버프/디버프 틀 보강
    대리자에서 버프디버프 클래스 스스로 받는 부분 추가
    
    구상된 버프/디버프 종류 중 포괄적인 개념은 포함하게 변경

--- Assets/Script/BuffDebuff.cs
+++ Assets/Script/BuffDebuff.cs
@@ -5,13 +5,18 @@ using UnityEngine;
 
 public class BuffDebuff
 {
-    public float SpeedVariance = 0;
+    public float SpeedRatio = 0;
     public float DamagePerSec = 0;
+    public float AddPowerRatio = 0;
+    public float DamageRatio = 0;
+    public float StaminaRecover = 0;
+    public float AtackSpeedRatio;
+
     public float Timer = 0;
     public bool IsBuff = false;
     public GameObject Attached;
 
-    public delegate void DelegateFunc(GameObject Target);
+    public delegate void DelegateFunc(GameObject Target, BuffDebuff state);
     public List<DelegateFunc> DelegateContainers = new();
 
     private float DamageCalculator = 0;
@@ -34,12 +39,12 @@ public class BuffDebuff
 
         if (Attached.GetComponent<Rigidbody2D>() != null) {
             Vector2 originVal = Attached.GetComponent<Rigidbody2D>().velocity;
-            Attached.GetComponent<Rigidbody2D>().velocity = new Vector2(originVal.x * (1 + SpeedVariance), originVal.y);
+            Attached.GetComponent<Rigidbody2D>().velocity = new Vector2(originVal.x * (1 + SpeedRatio), originVal.y);
         }
 
         foreach (var container in DelegateContainers) {
 
-            container(Attached);
+            container(Attached, this);
         }
 
         return true;
