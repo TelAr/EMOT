4579e71f1be0670cd6ae0afbefe2b41d58721945
TelAr <qet112313@gmail.com>
2022-07-22 10:31:30 +0900

    :bug: 수정: 패션임팩트 돌진 횟수 제한 설정 적용

--- Assets/Script/Unit/Enemy/Passion/Pattern/PassionImpact.cs
+++ Assets/Script/Unit/Enemy/Passion/Pattern/PassionImpact.cs
@@ -25,54 +25,68 @@ public class PassionImpact : PatternDefault
     public GameObject GaugeObjectModel;
     public Vector3 GaugePosOffset;
 
+    public float PlayerStunTime = 2f;
+
     private float timer = 0;
     private float fixedTimer = 0;
     private int step = 0;
+    private int rushCounter = 0;
     private GameObject beam = null;
     private GameObject targettingSignObject = null;
     private GameObject gaugeObject = null;
     private Vector3 nomalVec = Vector3.zero;
-    private float rushLeng=0;
+    private float rushLeng = 0;
     private float nowGauge;
     private Vector3 impactPoint;
     private bool isSpecialParrying = false;
+
     public override void Setting()
     {
         timer = 0;
         step = 0;
-        if (targettingSignObject == null) {
-
+        rushCounter = 0;
+        isSpecialParrying = false;
+        if (targettingSignObject == null)
+        {
             targettingSignObject = Instantiate(TargettingSignModel);
             targettingSignObject.GetComponent<SpriteRenderer>().color = TargettingSignColor;
         }
         targettingSignObject.SetActive(false);
 
-        if(gaugeObject == null)
+        if (gaugeObject == null)
         {
             gaugeObject = Instantiate(GaugeObjectModel);
         }
         gaugeObject.SetActive(false);
     }
 
-
-
     public override void Run()
     {
         base.Run();
 
         float maxDistance = 0;
-        Vector3 targetPosition= OriginPosition;
-        foreach (var pair in PatternPositionList) 
+        Vector3 targetPosition = OriginPosition;
+        foreach (var pair in PatternPositionList)
         {
-            if (maxDistance < (pair - GameController.GetPlayer.GetComponent<PlayerPhysical>().TargettingPos).magnitude) 
+            if (
+                maxDistance
+                < (
+                    pair - GameController.GetPlayer.GetComponent<PlayerPhysical>().TargettingPos
+                ).magnitude
+            )
             {
-                maxDistance = (pair - GameController.GetPlayer.GetComponent<PlayerPhysical>().TargettingPos).magnitude;
+                maxDistance = (
+                    pair - GameController.GetPlayer.GetComponent<PlayerPhysical>().TargettingPos
+                ).magnitude;
                 targetPosition = pair;
             }
         }
 
-        gameObject.GetComponent<Rigidbody2D>().velocity = Ballistics.Ballistic(targetPosition - gameObject.transform.position,
-           JumpSpeed, GameController.GetGameController.GRAVITY);
+        gameObject.GetComponent<Rigidbody2D>().velocity = Ballistics.Ballistic(
+            targetPosition - gameObject.transform.position,
+            JumpSpeed,
+            GameController.GetGameController.GRAVITY
+        );
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -82,69 +96,77 @@ public class PassionImpact : PatternDefault
             gameObject.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
             if (step == 0)
             {
-                step = 1;
-                timer = 0;
-                targettingSignObject.GetComponent<SpriteRenderer>().color = TargettingSignColor - new Color(0, 0, 0, 1);
+                TryRush();
+                targettingSignObject.GetComponent<SpriteRenderer>().color =
+                    TargettingSignColor - new Color(0, 0, 0, 1);
                 targettingSignObject.SetActive(true);
             }
-            else if (step == 2) {
-
-                step = 1;
+            else if (step == 2)
+            {
+                TryRush();
             }
         }
-
     }
 
     public override void Stop()
     {
         base.Stop();
         gaugeObject.SetActive(false);
+        targettingSignObject.SetActive(false);
         ReturnBeam();
     }
+
     // Update is called once per frame
     void Update()
     {
-        if (IsRun) {
-
-            if (beam != null && beam.activeSelf) {
-
-                beam.GetComponent<LineRenderer>().SetPosition(1, transform.position + new Vector3(0, 0, 1));
+        if (IsRun)
+        {
+            float ratio;
+            if (beam != null && beam.activeSelf)
+            {
+                beam.GetComponent<LineRenderer>()
+                    .SetPosition(1, transform.position + new Vector3(0, 0, 1));
             }
 
             timer += Time.deltaTime;
-            switch (step) { 
-            
+            switch (step)
+            {
                 case 0:
                     //before landing step
                     break;
                 case 1:
-                    float ratio = timer / TargettingTime;
-                    if (ratio < 1) {
-
-                        targettingSignObject.GetComponent<SpriteRenderer>().color = TargettingSignColor - new Color(0, 0, 0, 0.5f - ratio);
-                        targettingSignObject.transform.localScale = TargettingSignModel.transform.localScale * (1 - ratio);
-                        targettingSignObject.transform.position = GameController.GetPlayer.GetComponent<PlayerPhysical>().TargettingPos;
-                        if (beam != null && beam.activeSelf)
-                        {
-                            beam.GetComponent<LineRenderer>().endWidth= BeamMaxWidth*(1-ratio);
-                        }
+                    ratio = timer / TargettingTime;
+                    if (ratio < 1)
+                    {
+                        targettingSignObject.GetComponent<SpriteRenderer>().color =
+                            TargettingSignColor - new Color(0, 0, 0, 0.5f - ratio);
+                        targettingSignObject.transform.localScale =
+                            TargettingSignModel.transform.localScale * (1 - ratio);
+                        targettingSignObject.transform.position = GameController.GetPlayer
+                            .GetComponent<PlayerPhysical>()
+                            .TargettingPos;
+                        BeamEffect(ratio);
                     }
-                    else 
+                    else
                     {
                         ReturnBeam();
                         SetBeam();
                         if (GameController.GetPlayer.GetComponent<PlayerAction>().IsParrying())
                         {
-                            GameController.GetPlayer.GetComponent<PlayerAudio>().ParryingSuccessPlay();
+                            GameController.GetPlayer
+                                .GetComponent<PlayerAudio>()
+                                .ParryingSuccessPlay();
                             nowGauge = GaugeAmont * 0.5f;
-                            impactPoint = GameController.GetPlayer.GetComponent<PlayerPhysical>().TargettingPos;
+                            impactPoint = GameController.GetPlayer
+                                .GetComponent<PlayerPhysical>()
+                                .TargettingPos;
 
-                            GameController.GetPlayer.transform.position = impactPoint + new Vector3(1, 0, 0);
+                            GameController.GetPlayer.transform.position =
+                                impactPoint + new Vector3(1, 0, 0);
                             transform.position = impactPoint + new Vector3(-1, 0, 0);
 
                             gaugeObject.transform.position = impactPoint + GaugePosOffset;
                             gaugeObject.SetActive(true);
-                            
 
                             GameController.GetPlayer.GetComponent<PlayerPhysical>().Bind(100);
                             step = 3;
@@ -152,11 +174,20 @@ public class PassionImpact : PatternDefault
                         else
                         {
                             GameController.GetPlayer.GetComponent<PlayerHealth>().Hurt(20, 0);
-                            nomalVec = (GameController.GetPlayer.GetComponent<PlayerPhysical>().TargettingPos - gameObject.transform.position).normalized;
-                            rushLeng = Mathf.Max(RushMaxLength,
-                                (GameController.GetPlayer.GetComponent<PlayerPhysical>().TargettingPos - gameObject.transform.position).magnitude);
+                            nomalVec = (
+                                GameController.GetPlayer
+                                    .GetComponent<PlayerPhysical>()
+                                    .TargettingPos - gameObject.transform.position
+                            ).normalized;
+                            rushLeng = Mathf.Max(
+                                RushMaxLength,
+                                (
+                                    GameController.GetPlayer
+                                        .GetComponent<PlayerPhysical>()
+                                        .TargettingPos - gameObject.transform.position
+                                ).magnitude
+                            );
 
-                            
                             fixedTimer = 0;
                             step = 2;
                         }
@@ -172,12 +203,11 @@ public class PassionImpact : PatternDefault
 
                     //player action logic
                     nowGauge += GaugeRecoverPerSec * Time.deltaTime;
-                    if (isSpecialParrying) {
-
+                    if (isSpecialParrying)
+                    {
                         nowGauge -= GaugeDamagePerButton;
                         GameController.GetPlayer.GetComponent<PlayerAudio>().ParryingSuccessPlay();
                         isSpecialParrying = false;
-
                     }
 
                     gaugeObject.GetComponent<Gauge>().GaugeValue = nowGauge / GaugeAmont;
@@ -186,12 +216,30 @@ public class PassionImpact : PatternDefault
                     {
                         //success event
                         GameController.GetPlayer.GetComponent<PlayerPhysical>().BindFree();
-                        Stop();
+                        step = 4;
                     }
-                    if (nowGauge > GaugeAmont) {
-
+                    if (nowGauge > GaugeAmont)
+                    {
                         //fail event
-                        GameController.GetPlayer.GetComponent<PlayerPhysical>().Bind(10);
+                        GameController.GetPlayer.GetComponent<PlayerPhysical>().BindFree();
+                        /*
+                         * stun debuff on
+                        GameController.GetPlayer
+                            .GetComponent<PlayerPhysical>()
+                            .Bind(PlayerStunTime);
+                        */
+                        step = 4;
+                    }
+
+                    break;
+                case 4:
+                    ratio = timer / TargettingTime;
+                    if (ratio < 1)
+                    {
+                        BeamEffect(ratio);
+                    }
+                    else
+                    {
                         Stop();
                     }
 
@@ -202,12 +250,12 @@ public class PassionImpact : PatternDefault
 
     private void FixedUpdate()
     {
-
         if (IsRun)
         {
             if (step == 0)
             {
-                gameObject.GetComponent<Rigidbody2D>().velocity += new Vector2(0, GameController.GetGameController.GRAVITY) * Time.fixedDeltaTime;
+                gameObject.GetComponent<Rigidbody2D>().velocity +=
+                    new Vector2(0, GameController.GetGameController.GRAVITY) * Time.fixedDeltaTime;
             }
             else if (step == 2)
             {
@@ -217,20 +265,19 @@ public class PassionImpact : PatternDefault
                 if (fixedTimer > rushLeng / RushSpeed)
                 {
                     gameObject.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
-                    step = 1;
+                    TryRush();
                 }
             }
-            else {
-
+            else
+            {
                 gameObject.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
             }
         }
     }
 
-
-    private void SetBeam() {
-
-        GameObject retObject=null;
+    private void SetBeam()
+    {
+        GameObject retObject = null;
 
         retObject = EffectPoolingController.Instance.GetLineRenderer();
         LineRenderer lr = retObject.GetComponent<LineRenderer>();
@@ -240,12 +287,11 @@ public class PassionImpact : PatternDefault
         lr.SetPosition(0, transform.position + new Vector3(0, 0, 1));
         lr.SetPosition(1, transform.position + new Vector3(0, 0, 1));
         lr.numCapVertices = 10;
-        beam =  retObject;
+        beam = retObject;
     }
 
     private void ReturnBeam()
     {
-
         if (beam != null)
         {
             beam.SetActive(false);
@@ -254,9 +300,31 @@ public class PassionImpact : PatternDefault
 
     public void OnParrying()
     {
-        if (step==3)
+        if (step == 3)
         {
             isSpecialParrying = true;
+        }
+    }
+
+    private void TryRush()
+    {
+        timer = 0;
+        if (rushCounter < RushCount)
+        {
+            rushCounter++;
+            step = 1;
+        }
+        else
+        {
+            step = 4;
+        }
+    }
+
+    private void BeamEffect(float ratio)
+    {
+        if (beam != null && beam.activeSelf)
+        {
+            beam.GetComponent<LineRenderer>().endWidth = BeamMaxWidth * (1 - ratio);
         }
     }
 }
