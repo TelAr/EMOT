8036b387b1e1f13e9a6b7b5a885354ccbb0f3e76
Tear <qet112313@gmail.com>
2022-07-25 18:40:10 +0900

    :bug: 수정: 사이킥 익스플루젼 잔류 현상 수정

--- Assets/Script/Unit/Enemy/Share/Explosion.cs
+++ Assets/Script/Unit/Enemy/Share/Explosion.cs
@@ -4,26 +4,28 @@ using UnityEngine;
 
 public class Explosion : MonoBehaviour
 {
-
     public float RemainTime;
     public float FadeOutTime;
     public bool test = false;
-    public float JudgeStartRadius, JudgeEndRadius;
+    public float JudgeStartRadius,
+        JudgeEndRadius;
     public float Judgetime;
     public AudioClip DefaultSound;
     public float ExplosionVolumeOffset = 1;
 
     private Animator animator = null;
     private CircleCollider2D circleCollider;
-    private AudioSource audioSource=null;
-    private bool animation_end, isPlay;
+    private AudioSource audioSource = null;
+    private bool animation_end,
+        isPlay;
+
     // Start is called before the first frame update
     void Awake()
     {
-
         animator = gameObject.GetComponent<Animator>();
         circleCollider = gameObject.GetComponent<CircleCollider2D>();
-        if (gameObject.GetComponent<AudioSource>()) {
+        if (gameObject.GetComponent<AudioSource>())
+        {
             audioSource = gameObject.GetComponent<AudioSource>();
         }
         Initiation();
@@ -38,62 +40,62 @@ public class Explosion : MonoBehaviour
         gameObject.GetComponent<SpriteRenderer>().color = Color.white;
         if (animator == null)
         {
-
             animator = gameObject.GetComponent<Animator>();
         }
         transform.rotation = Quaternion.Euler(new Vector3(0, 0, Random.Range(0, 360)));
     }
 
-    public void Initiation(float SoundVolume = 1f, AudioClip clip=null) {
-
+    public void Initiation(float SoundVolume = 1f, AudioClip clip = null)
+    {
         if (audioSource != null)
         {
             if (clip == null)
             {
-
                 audioSource.clip = DefaultSound;
             }
             else
             {
-
                 audioSource.clip = clip;
             }
             isPlay = true;
-            audioSource.volume = ExplosionVolumeOffset * AudioDefault.MasterVolume * AudioDefault.EffectVolume * SoundVolume;
+            audioSource.volume =
+                ExplosionVolumeOffset
+                * AudioDefault.MasterVolume
+                * AudioDefault.EffectVolume
+                * SoundVolume;
         }
-
     }
 
     private void FixedUpdate()
     {
         if (animator.GetCurrentAnimatorStateInfo(0).IsName("Explosion"))
         {
-
             if (animator.GetCurrentAnimatorStateInfo(0).normalizedTime < Judgetime)
             {
-                circleCollider.radius = JudgeStartRadius * (Judgetime - animator.GetCurrentAnimatorStateInfo(0).normalizedTime) / Judgetime +
-                    JudgeEndRadius * animator.GetCurrentAnimatorStateInfo(0).normalizedTime / Judgetime;
-
+                circleCollider.radius =
+                    JudgeStartRadius
+                        * (Judgetime - animator.GetCurrentAnimatorStateInfo(0).normalizedTime)
+                        / Judgetime
+                    + JudgeEndRadius
+                        * animator.GetCurrentAnimatorStateInfo(0).normalizedTime
+                        / Judgetime;
             }
         }
-
     }
 
     // Update is called once per frame
     void Update()
     {
-
-        if(isPlay&& audioSource!=null)
+        if (isPlay && audioSource != null)
         {
             audioSource.Play();
             isPlay = false;
         }
 
-        if (animator.GetCurrentAnimatorStateInfo(0).IsName("Explosion")) {
-
+        if (animator.GetCurrentAnimatorStateInfo(0).IsName("Explosion"))
+        {
             if (animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= Judgetime)
             {
-
                 if (animator.GetCurrentAnimatorStateInfo(0).normalizedTime < 1.0f)
                 {
                     gameObject.GetComponent<Damage>().IsEffected = false;
@@ -101,20 +103,22 @@ public class Explosion : MonoBehaviour
                 }
                 else
                 {
-
                     gameObject.GetComponent<SpriteRenderer>().color = Color.clear;
                     animation_end = true;
                 }
             }
-            
         }
 
-        if (animation_end && (audioSource!=null &&!audioSource.isPlaying)) { 
-        
+        if (animator.GetCurrentAnimatorStateInfo(0).IsName("End"))
+        {
+            gameObject.GetComponent<SpriteRenderer>().color = Color.clear;
+            animation_end = true;
+        }
+
+        if (animation_end && (audioSource == null || !audioSource.isPlaying))
+        {
             gameObject.SetActive(false);
         }
-
-
 
         //LAGACY
         /*
@@ -123,7 +127,7 @@ public class Explosion : MonoBehaviour
 
             gameObject.tag = "Untagged";
             gameObject.GetComponent<SpriteRenderer>().color=new Color(color.r,color.g,color.b,(FadeOutTime+RemainTime-timer)/FadeOutTime);
-            if (FadeOutTime + RemainTime < timer) { 
+            if (FadeOutTime + RemainTime < timer) {
             
                 Destroy(gameObject);
             }
